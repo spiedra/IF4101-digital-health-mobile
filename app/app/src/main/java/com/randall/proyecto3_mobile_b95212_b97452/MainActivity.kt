093eb5372a21139ea571795ed9714f70db9bb561
Randall <randms07@hotmail.com>
2021-07-13 22:24:07 -0600

    agregando app proyecto

---
+++ app/app/src/main/java/com/randall/proyecto3_mobile_b95212_b97452/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.randall.proyecto3_mobile_b95212_b97452
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
