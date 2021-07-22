619664a914d14f0c51b2d3b85e09bd313024bbf6
Randall <randms07@hotmail.com>
2021-07-21 19:48:39 -0600

    config de red

--- app/src/main/java/com/randalljuan/proyecto3_mobile_b95212_b97452/core/RetrofitHelper.kt
+++ app/src/main/java/com/randalljuan/proyecto3_mobile_b95212_b97452/core/RetrofitHelper.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitHelper {
     fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
-            .baseUrl("http://192.168.0.6/apiLocalhost/")
+            .baseUrl("http://192.168.1.2/apiLocalhost/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
